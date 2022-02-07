5a836b16e909bb5a5088755b988444ab4fca78e2
Ivan Savchyn <92987226+IvanSavchyn194@users.noreply.github.com>
2022-02-07 18:05:00 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-sudo systemctl stop eschool.service
-rm -r /home/azureuser/eschool/eschool.jar
-wget -P /home/azureuser/eschool/ https://eschoolstorage.blob.core.windows.net/eschool/eschool.jar
+sudo apt update
+sudo apt install -y openjdk-8-jdk
+mkdir /home/azureuser/eschool
+sudo chown -R azureuser:azureuser /home/azureuser/eschool
+wget -P /home/azureuser/eschool https://eschoolstorage.blob.core.windows.net/eschool/eschool.jar
+sudo wget -P /etc/systemd/system https://eschoolstorage.blob.core.windows.net/eschool/eschool.service
+sudo systemctl daemon-reload
+sudo systemctl enable eschool.service
 sudo systemctl start eschool.service
+
